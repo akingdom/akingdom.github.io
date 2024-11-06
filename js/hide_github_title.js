ab486c3b686686a5fed251015e06e305a0d8d11a
Andrew Kingdom <1809762+akingdom@users.noreply.github.com>
2024-11-06 13:22:54 +1100

    Update hide_github_title.js

--- js/hide_github_title.js
+++ js/hide_github_title.js
@@ -1,3 +1,3 @@
-
-  let h1_0 = document.getElementsByTagName('h1')[0]?.getElementsByTagName('a')[0];
-  if(h1_0) h1_0.textContent = '';
+// Use this near at the end of the file.
+let h1_0 = document.getElementsByTagName('h1')[0]?.getElementsByTagName('a')[0];
+if(h1_0) h1_0.textContent = '';
